25cadb0b3473e44ac0554ea741d2e784d00f1d92
aniversarioperu <aniversarioperu1@gmail.com>
2014-03-22 16:13:46 +0200

    red line or grey line

--- distribution.R
+++ distribution.R
@@ -14,17 +14,6 @@ fit5 <- lm( y~ns(x, 3) )
 fit6 <- lm( y~ns(x, 9) )
 
 
-plot(x, y, xlim=c(0,x[length(x)]*10), ylim=c(0,y[length(y)]*10), pch=19)
-
-xx <- seq(0,x[length(x)]*10, length.out=250)
-lines(xx, predict(fit1, data.frame(x=xx)), lwd=2, col='blue')
-lines(xx, predict(fit2, data.frame(x=xx)), lwd=2, col='green')
-lines(xx, predict(fit3, data.frame(x=xx)), lwd=2, col='red')
-lines(xx, predict(fit4, data.frame(x=xx)), lwd=2, col='purple')
-lines(xx, predict(fit5, data.frame(x=xx)), lwd=2, col='orange')
-lines(xx, predict(fit6, data.frame(x=xx)), lwd=2, col='grey')
-
-
 
 plot(x, y, xlim=c(0,x[length(x)]), ylim=c(0,y[length(y)]), pch=19)
 
@@ -35,4 +24,16 @@ lines(xx, predict(fit3, data.frame(x=xx)), lwd=2, col='red')
 lines(xx, predict(fit4, data.frame(x=xx)), lwd=2, col='purple')
 lines(xx, predict(fit5, data.frame(x=xx)), lwd=2, col='orange')
 lines(xx, predict(fit6, data.frame(x=xx)), lwd=2, col='grey')
+
+
+plot(x, y, xlim=c(0,x[length(x)]*10), ylim=c(0,y[length(y)]*10), pch=19)
+
+xx <- seq(0,x[length(x)]*10, length.out=250)
+lines(xx, predict(fit1, data.frame(x=xx)), lwd=2, col='blue')
+lines(xx, predict(fit2, data.frame(x=xx)), lwd=2, col='green')
+lines(xx, predict(fit3, data.frame(x=xx)), lwd=2, col='red')
+lines(xx, predict(fit4, data.frame(x=xx)), lwd=2, col='purple')
+lines(xx, predict(fit5, data.frame(x=xx)), lwd=2, col='orange')
+lines(xx, predict(fit6, data.frame(x=xx)), lwd=2, col='grey')
+
 
