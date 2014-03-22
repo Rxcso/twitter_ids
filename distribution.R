eaa085d5b446243acd111ab38d0ab05084fd5413
aniversarioperu <aniversarioperu1@gmail.com>
2014-03-22 15:30:20 +0200

    find distribution using R

---
+++ distribution.R
@@ -0,0 +1,3 @@
+
+data <- read.csv("output_r.csv", header=F)
+names(data) <- c("x", "y")
--- plot.py
+++ plot.py
@@ -5,8 +5,11 @@ import math
 import matplotlib.pyplot as plt
 import brewer2mpl
 import datetime
+import time
 import sys
 
+
+start_timestamp = time.mktime(datetime.datetime(2006,3,21).timetuple())
 x_axis = []
 y_axis = []
 
@@ -15,10 +18,11 @@ with open("output.csv", "r") as filename:
         i = i.strip().split(",")
         date = datetime.datetime.strptime(i[0], "%Y-%m-%d %H:%M:%S")
         tweet_id = i[1]
+        print int(time.mktime(date.timetuple()) - start_timestamp), tweet_id
 
         x_axis.append(date)
-        y_axis.append(float(tweet_id))
-        #y_axis.append(math.log(float(tweet_id)))
+        #y_axis.append(float(tweet_id))
+        y_axis.append(math.log(float(tweet_id)))
 
 # queremos color
 set2 = brewer2mpl.get_map('Set2', 'qualitative', 8).mpl_colors
